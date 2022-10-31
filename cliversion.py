4363350b18ef1259286d9e1419d32bbfdc83d9fd
john <97820338+johm01@users.noreply.github.com>
2022-10-31 02:14:41 -0500

    Update cliversion.py

--- cliversion.py
+++ cliversion.py
@@ -18,3 +18,19 @@ def draw_tree(level,size,angel):
     left(angel * 2)
     draw_tree(level - 1,size * 0.8,angel)
     backward(size)
+ 
+def snowflake(size,levels,shorten_factor,angle):
+    if n == 0:
+        forward(size)
+    else:
+        n -= 1
+ 
+    
+        size = size / shorten_factor
+        snowflake(size ,levels,shorten_factor,angle)
+        left(angle)
+        snowflake(size ,levels,shorten_factor,angle)
+        right(angle * 2)
+        snowflake(size ,levels,shorten_factor,angle)
+        left(angle)
+        snowflake(size ,levels,shorten_factor,angle)
